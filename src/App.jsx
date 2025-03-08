f16b68310c4b459f228931d75632a9cddfd41761
Shreya Kulkarni <shreya.kulkarni132@gmail.com>
2025-03-09 00:02:25 +0530

    desktop-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import Todolist from './todolist.jsx'
+
+function App() {
+  return (
+    <div>
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
